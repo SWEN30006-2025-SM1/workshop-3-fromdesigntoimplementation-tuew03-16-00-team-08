ae235ead6f1a58a2388072660fc4d09241cbc3f9
Avjay <abhar@student.unimelb.edu.au>
2025-03-25 17:47:48 +1100

    Implement basic Person/Staff/Student information

--- app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
+++ app/src/main/java/com/unimelb/swen30006/workshops/Submission.java
@@ -2,6 +2,37 @@ package com.unimelb.swen30006.workshops;
 
 // Sample private submission class to be replaced by your implementation
 class Submission {
+
+    private int attemptNum ;
+
+    private File[] files;
+
+    private int numFiles;
+
+    public int getAttemptNum() {
+        return attemptNum;
+    }
+
+    public void setAttemptNum(int attemptNum) {
+        this.attemptNum = attemptNum;
+    }
+
+    public File[] getFiles() {
+        return files;
+    }
+
+    public void setFiles(File[] files) {
+        this.files = files;
+    }
+
+    public int getNumFiles() {
+        return numFiles;
+    }
+
+    public void setNumFiles(int numFiles) {
+        this.numFiles = numFiles;
+    }
+
     // Return two fake files
     public File[] includedFiles(){
         File[] files = new File[2];
